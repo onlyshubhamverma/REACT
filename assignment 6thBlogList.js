e793898b41007ec338f570cd1b126cf95d69353d
onlyshubhamverma <102594568+onlyshubhamverma@users.noreply.github.com>
2022-05-24 02:15:19 +0530

    Add files via upload

---
+++ assignment 6thBlogList.js
@@ -0,0 +1,10 @@
+const BlogList = (props) => {
+    const title ="Welcome to Main page"
+    return ( 
+        <div className="create">
+            <h2>{title}</h2>
+        </div>
+     );
+}
+ 
+export default BlogList;
